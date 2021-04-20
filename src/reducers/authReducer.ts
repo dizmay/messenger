cb0922f2b42f6fe74814e6a1a2929c7d42360f66
Piotr Stukalau <Piotr.Stukalau@itechart-group.com>
2021-04-20 16:11:38 +0300

    refactor: delete necessary code

--- src/reducers/authReducer.ts
+++ src/reducers/authReducer.ts
@@ -11,7 +11,6 @@ export interface AuthStateType {
   isLoading: boolean,
 }
 
-
 const initialState: AuthStateType = {
   isLogged: null,
   isError: false,
@@ -21,47 +20,6 @@ const initialState: AuthStateType = {
   username: '',
   isLoading: false,
 }
-
-// const authReducer = (state = initialState, action: PayloadAction<ResponseUserData>): AuthStateType => {
-//   switch (action.type) {
-
-//     case types.REGISTRATION:
-//       return { ...state, isLoading: true, message: '' };
-
-//     case types.REGISTRATION_SUCCESS:
-//       return { ...state, isLoading: false, id: action.payload.id, email: action.payload.email, username: action.payload.username, isLogged: true, isError: false };
-
-//     case types.REGISTRATION_FAILED:
-//       return { ...state, isLoading: false, message: action.payload, isLogged: false, isError: true };
-
-//     case types.LOGIN:
-//       return { ...state, isLoading: true, message: '' };
-
-//     case types.LOGIN_SUCCESS:
-//       return { ...state, isLoading: false, id: action.payload.id, email: action.payload.email, username: action.payload.username, isLogged: true, isError: false };
-
-//     case types.LOGIN_FAILED:
-//       return { ...state, isLoading: false, message: action.payload, isLogged: false, isError: true };
-
-//     case types.SET_CURRENT_USER:
-//       return { ...state, isLoading: true }
-
-//     case types.SET_CURRENT_USER_SUCCESS:
-//       return { ...state, isLoading: false, id: action.payload.id, email: action.payload.email, username: action.payload.username, isLogged: true, message: '', isError: false }
-
-//     case types.SET_CURRENT_USER_FAILED:
-//       return { ...state, isLoading: false, isLogged: false, isError: true }
-
-//     case types.LOGOUT:
-//       return { ...state, isLoading: true };
-
-//     case types.LOGOUT_SUCCESS:
-//       return { ...state, isLoading: false, id: null, email: '', username: '', isLogged: false, message: '' };
-
-//     default:
-//       return state;
-//   }
-// }
 
 const authSlice = createSlice({
   name: 'auth',
